c38312633484b17c5c9c3593f9fa142b97eae76c
SachinHore <sachin.hore97@gmail.com>
2022-11-30 22:21:09 +0530

    First

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <div class="alert alert-primary" role="alert">
+        <h1>Home Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
